28d8578d1fb5831950d88539ade9177d1ad44c94
Win Feigle <win.feigle@gmail.com>
2022-06-08 19:04:49 -0600

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your code here
+//Helper function to create an li
+const createUser = (data) => {
+    let li = document.createElement('li')
+    li.textContent = `${data}`
+    document.body.appendChild(li)
+}
+
+const submitData = (userName, userEmail) => {
+    return fetch('http://localhost:3000/users', {
+        method:'POST',
+        headers: {
+            'Content-Type':'application/json',
+            'Accept':'application/json'
+        },
+        body: JSON.stringify({
+            'name': userName,
+            'email': userEmail
+        })
+    })
+    .then(res => res.json())
+    .then(data => {createUser(data.id)})
+    .catch(error => document.body.innerHTML = error)
+}
+
+submitData();
